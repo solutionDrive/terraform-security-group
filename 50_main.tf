eb90face26a39d571e8c3d185530a768ef026f61
Tobias Lückel <tobias.lueckel@netlogix.de>
2019-12-13 15:49:11 +0100

    Use 'dynamic' to be able to set correct cidr_blocks and description

--- 50_main.tf
+++ 50_main.tf
@@ -20,9 +20,14 @@ resource "aws_security_group_rule" "cidr_ipv4_ingress_rule" {
   to_port = var.cidr_ipv4_ingress_rules["ports"][each.key]
   protocol = var.cidr_ipv4_ingress_rules["protocols"][each.key]
 
-  cidr_blocks = split(",", var.cidr_ipv4_ingress_rules["cidr_blocks"][each.key])
+  dynamic "cidr_block" {
+    for_each = split(",", var.cidr_ipv4_ingress_rules["cidr_blocks"][each.key])
+    content {
+      cidr_blocks = cidr_block.value
 
-  description = element(split(",", var.cidr_ipv4_ingress_rules["descriptions"][each.key]))
+      description = element(split(",", var.cidr_ipv4_ingress_rules["descriptions"][each.key]), cidr_block.key)
+    }
+  }
 }
 
 resource "aws_security_group_rule" "cidr_ipv4_egress_rule" {
@@ -37,9 +42,14 @@ resource "aws_security_group_rule" "cidr_ipv4_egress_rule" {
   to_port = var.cidr_ipv4_egress_rules["ports"][each.key]
   protocol = var.cidr_ipv4_egress_rules["protocols"][each.key]
 
-  cidr_blocks = split(",", var.cidr_ipv4_egress_rules["cidr_blocks"][each.key])
+  dynamic "cidr_block" {
+    for_each = split(",", var.cidr_ipv4_egress_rules["cidr_blocks"][each.key])
+    content {
+      cidr_blocks = cidr_block.value
 
-  description = element(split(",", var.cidr_ipv4_egress_rules["descriptions"][each.key]))
+      description = element(split(",", var.cidr_ipv4_egress_rules["descriptions"][each.key]), cidr_block.key)
+    }
+  }
 }
 
 ##### IPv6 #####
@@ -55,9 +65,14 @@ resource "aws_security_group_rule" "cidr_ipv6_ingress_rule" {
   to_port = var.cidr_ipv6_ingress_rules["ports"][each.key]
   protocol = var.cidr_ipv6_ingress_rules["protocols"][each.key]
 
-  ipv6_cidr_blocks = split(",", var.cidr_ipv6_ingress_rules["ipv6_cidr_blocks"][each.key])
+  dynamic "cidr_block" {
+    for_each = split(",", var.cidr_ipv6_ingress_rules["cidr_blocks"][each.key])
+    content {
+      ipv6_cidr_blocks = cidr_block.value
 
-  description = element(split(",", var.cidr_ipv6_ingress_rules["descriptions"][each.key]))
+      description = element(split(",", var.cidr_ipv6_ingress_rules["descriptions"][each.key]), cidr_block.key)
+    }
+  }
 }
 
 resource "aws_security_group_rule" "cidr_ipv6_egress_rule" {
@@ -72,9 +87,14 @@ resource "aws_security_group_rule" "cidr_ipv6_egress_rule" {
   to_port = var.cidr_ipv6_egress_rules["ports"][each.key]
   protocol = var.cidr_ipv6_egress_rules["protocols"][each.key]
 
-  ipv6_cidr_blocks = split(",", var.cidr_ipv6_egress_rules["ipv6_cidr_blocks"][each.key])
+  dynamic "cidr_block" {
+    for_each = split(",", var.cidr_ipv6_egress_rules["cidr_blocks"][each.key])
+    content {
+      ipv6_cidr_blocks = cidr_block.value
 
-  description = element(split(",", var.cidr_ipv6_egress_rules["descriptions"][each.key]))
+      description = element(split(",", var.cidr_ipv6_egress_rules["descriptions"][each.key]), cidr_block.key)
+    }
+  }
 }
 
 ##### SecurityGroups #####
@@ -92,7 +112,7 @@ resource "aws_security_group_rule" "sg_ingress_rule" {
 
   source_security_group_id = var.security_group_ingress_rules["source_security_groups"][each.key] == "self" ? aws_security_group.security_group.id : var.security_group_ingress_rules["source_security_groups"][each.key]
 
-  description = element(split(",", var.security_group_ingress_rules["descriptions"][each.key]))
+  description = element(split(",", var.security_group_ingress_rules["descriptions"][each.key]), each.key)
 }
 
 resource "aws_security_group_rule" "sg_egress_rule" {
@@ -109,5 +129,5 @@ resource "aws_security_group_rule" "sg_egress_rule" {
 
   source_security_group_id = var.security_group_egress_rules["source_security_groups"][each.key] == "self" ? aws_security_group.security_group.id : var.security_group_egress_rules["source_security_groups"][each.key]
 
-  description = element(split(",", var.security_group_egress_rules["descriptions"][each.key]))
+  description = element(split(",", var.security_group_egress_rules["descriptions"][each.key]), each.key)
 }
